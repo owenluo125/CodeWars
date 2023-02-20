6cf07b6e0cbe712471ab88964dc229dcc6052cd3
swichherddhhhh <31743891+swichherddhhhh@users.noreply.github.com>
2023-02-20 16:54:37 -0700

    Create [8] Grasshopper - Terminal Game #1.py

---
+++ February 2023/[8] Grasshopper - Terminal Game #1.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/55e8aba23d399a59500000ce/python
+
+class Hero(object):
+    def __init__(self, name='Hero'):
+        self.name = name
+        self.position = '00'
+        self.health = 100
+        self.damage = 5
+        self.experience = 0
