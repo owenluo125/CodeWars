f303f77024771cdf0223abfa4d124384a5df5aee
swichherddhhhh <31743891+swichherddhhhh@users.noreply.github.com>
2023-02-20 17:19:22 -0700

    Create [8] Unfinished Loop - Bug Fixing #1.py

---
+++ February 2023/[8] Unfinished Loop - Bug Fixing #1.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/55c28f7304e3eaebef0000da/python
+
+def create_array(n):
+    res = []
+    i = 1
+    while i <= n: 
+        res.append(i)
+        i += 1
+    return res
