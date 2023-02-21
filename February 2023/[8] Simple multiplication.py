a6b4354a41b9528264c22e700fdbb1c2ff7a57ba
swichherddhhhh <31743891+swichherddhhhh@users.noreply.github.com>
2023-02-20 17:26:36 -0700

    Create [8] Simple multiplication.py

---
+++ February 2023/[8] Simple multiplication.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/583710ccaa6717322c000105/python
+
+def simple_multiplication(number):
+    if number % 2 == 0:
+        return number * 8
+    else:
+        return number * 9
