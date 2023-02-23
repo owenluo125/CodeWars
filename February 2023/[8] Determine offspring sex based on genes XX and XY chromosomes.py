674b6b864768ecbec2e8a803bf756715ef4ef960
swichherddhhhh <31743891+swichherddhhhh@users.noreply.github.com>
2023-02-23 12:41:04 -0700

    Create [8] Determine offspring sex based on genes XX and XY chromosomes.py

---
+++ February 2023/[8] Determine offspring sex based on genes XX and XY chromosomes.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/56530b444e831334c0000020/python
+
+def chromosome_check(sperm):
+    if sperm == "XX":
+        return "Congratulations! You\'re going to have a daughter."
+    else:
+        return "Congratulations! You\'re going to have a son."
