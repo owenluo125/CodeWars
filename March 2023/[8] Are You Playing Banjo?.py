c42ccfe5203758693b5961a496fe2f3a1457c2c7
swichherddhhhh <31743891+swichherddhhhh@users.noreply.github.com>
2023-03-04 23:06:07 -0700

    Create [8] Are You Playing Banjo?.py

---
+++ March 2023/[8] Are You Playing Banjo?.py
@@ -0,0 +1,4 @@
+# https://www.codewars.com/kata/53af2b8861023f1d88000832/python
+
+def are_you_playing_banjo(name):
+    return name + " plays banjo" if name[0].lower() == "r" else name + " does not play banjo"
