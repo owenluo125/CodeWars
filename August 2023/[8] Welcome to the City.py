eb9c3494921cc80ceeb0cc5c0a91310bb60bd21f
swichherddhhhh <31743891+swichherddhhhh@users.noreply.github.com>
2023-08-12 11:10:58 -0600

    Create [8] Welcome to the City.py

---
+++ August 2023/[8] Welcome to the City.py
@@ -0,0 +1,4 @@
+# https://www.codewars.com/kata/5302d846be2a9189af0001e4/python
+
+def say_hello(name, city, state):
+    return "Hello, " + ' '.join(name) + "! Welcome to " + city + ", " + state + "!"
