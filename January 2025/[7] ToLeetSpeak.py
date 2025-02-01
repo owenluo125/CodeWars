1a0895439ddf0c37fdab82d163b6a68f08804340
owenluo125 <31743891+owenluo125@users.noreply.github.com>
2025-01-31 23:43:42 -0700

    Create [7] ToLeetSpeak.py

---
+++ January 2025/[7] ToLeetSpeak.py
@@ -0,0 +1,34 @@
+# https://www.codewars.com/kata/57c1ab3949324c321600013f/python
+
+alphabet = {
+  "A" : '@',
+  "B" : '8',
+  "C" : '(',
+  "D" : 'D',
+  "E" : '3',
+  "F" : 'F',
+  "G" : '6',
+  "H" : '#',
+  "I" : '!',
+  "J" : 'J',
+  "K" : 'K',
+  "L" : '1',
+  "M" : 'M',
+  "N" : 'N',
+  "O" : '0',
+  "P" : 'P',
+  "Q" : 'Q',
+  "R" : 'R',
+  "S" : '$',
+  "T" : '7',
+  "U" : 'U',
+  "V" : 'V',
+  "W" : 'W',
+  "X" : 'X',
+  "Y" : 'Y',
+  "Z" : '2',
+  " " : ' ',
+}
+
+def to_leet_speak(s):
+    return "".join(alphabet[x] for x in s)
